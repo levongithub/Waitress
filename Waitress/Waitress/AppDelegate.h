dc4211c6a3f8edf5d762e059a72bf1bbdcadbde2
levongithub <leov.kirakosyan@gmail.com>
2015-12-11 20:18:20 +0400

    JUST DO IT ( storyboard removed ))))

---
+++ Waitress/Waitress/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Waitress
+//
+//  Created by Levon on 12/11/15.
+//  Copyright © 2015 Waitress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HomeViewController.h"
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HomeViewController *homeViewController;
+
+@end
+
---
+++ Waitress/Waitress/ViewControllers/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Waitress
+//
+//  Created by Levon on 12/11/15.
+//  Copyright © 2015 Waitress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
